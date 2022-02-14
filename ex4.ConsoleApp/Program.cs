75519fca00862a5a3424ddb59af743db87c72393
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-02-13 21:34:00 -0300

    ex4

--- ex4.ConsoleApp/Program.cs
+++ ex4.ConsoleApp/Program.cs
@@ -7,12 +7,30 @@ namespace ex4.ConsoleApp
         static void Main(string[] args)
         {
             int[] valores = new int[5];
+            int cont_negativos = 0, cont_positivos = 0;
+            int media_aritmetica = 0;
+            int somador=0;
 
-            for (int i = 1; i < valores.Length; i++)
+            for (int i = 0; i < valores.Length; i++)
             {
                 Console.Write("Digite o " + i + "º valor: ");
                 valores[i] = Convert.ToInt32(Console.ReadLine());
+
+                somador = somador + valores[i];
+                media_aritmetica = somador / valores.Length;
+
+                if (valores[i] < 0)
+                {
+                    cont_negativos++;
+                }
+                else
+                {
+                    cont_positivos++;
+                }
             }
+            Console.WriteLine("\r\nA média aritmética é: " + media_aritmetica);
+            Console.WriteLine("Quantidade de valores positivos: " + cont_positivos);
+            Console.WriteLine("Quantidade de valores negativos: " + cont_negativos);
         }
     }
 }
